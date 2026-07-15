f15f6beef9c008a7c4fbac1af1004b77b20cfa74
Syna00 <synast123@gmail.com>
2026-07-15 18:58:58 +0700

    first commit

---
+++ project_name_pastryshop_generator.py
@@ -0,0 +1,4 @@
+print("welcome to the pastry shop name generator0")
+yn = input("Siapa nama kamu? ")
+ywn = input("Siapa nama pasangan kamu? ")
+print("Nama toko kue kamu adalah: " + ywn + " & " + yn + " Shop")
---
+++ python_variables.py
@@ -0,0 +1,10 @@
+name = "Sinatrio"
+print(name)
+
+name = "Seto"
+print(name)
+
+#print(len(input("Nama kamu siapa?")))
+username = input("Nama kamu siapa? ")
+lenght = len(username)
+print(lenght)
